87d1ffe061ec942966a27459b99825440fd28d44
S0UL4 <62818840+S0UL4@users.noreply.github.com>
2024-07-01 19:37:57 +0100

    Update renamer_v11.py

--- renamer_v10.py
+++ renamer_v10.py
@@ -14,7 +14,7 @@ from Watermark import base64_bytes_watermark
 
 #GUI Params
 root = tk.Tk()
-root.title('Renamer v10')
+root.title('Renamer v11')
 root.resizable(False, False)
 root.geometry('700x350')
 
@@ -54,69 +54,74 @@ image_label.pack()
 
 #Function to modify a specific text field in a pdf page
 def search_and_replace(page,to_search_and_replace,one_time):
-
-    for b in page.get_text("dict",sort=True)["blocks"]:
-        for l in b["lines"]:
-            for s in l['spans']:
-                xl=Rect(s['bbox']).tl.x
-                yl=Rect(s['bbox']).tl.y
-                xr=Rect(s['bbox']).br.x
-                yr=Rect(s['bbox']).br.y     
-                font_size=yr-yl  
-                for to_search in list(to_search_and_replace.keys()):
-                    #print(s['text'])
-                    if(to_search in s['text']):
-                        text_lenght = fitz.get_text_length(to_search_and_replace[to_search], fontname="Courier", fontsize=10)
-                        rect_x2 = xl + text_lenght + 2  # needs margin
-                        rect_y2 = yl + font_size + 2  # needs margin
-                        Bbox_text=Rect(xl,yl,rect_x2,rect_y2)
-                        annot=page.add_redact_annot(Bbox_text, to_search_and_replace[to_search], fontname="Courier", fill=False,fontsize=font_size,align=TEXT_ALIGN_LEFT)  # more parameters
-                        if(one_time):
-                            return
-
+    try:
+        for b in page.get_text("dict",sort=True)["blocks"]:
+            for l in b["lines"]:
+                for s in l['spans']:
+                    xl=Rect(s['bbox']).tl.x
+                    yl=Rect(s['bbox']).tl.y
+                    xr=Rect(s['bbox']).br.x
+                    yr=Rect(s['bbox']).br.y     
+                    font_size=yr-yl  
+                    for to_search in list(to_search_and_replace.keys()):
+                        #print(s['text'])
+                        if(to_search in s['text']):
+                            text_lenght = fitz.get_text_length(to_search_and_replace[to_search], fontname="Courier", fontsize=10)
+                            rect_x2 = xl + text_lenght + 2  # needs margin
+                            rect_y2 = yl + font_size + 2  # needs margin
+                            Bbox_text=Rect(xl,yl,rect_x2,rect_y2)
+                            annot=page.add_redact_annot(Bbox_text, to_search_and_replace[to_search], fontname="Courier", fill=False,fontsize=font_size,align=TEXT_ALIGN_LEFT)  # more parameters
+                            if(one_time):
+                                return
+    except:
+        pass
 
 def run(doc,EIN,file_path,file_name):
     
     i=0
     
     EIN_masked=None
-    for page in doc:
-        for l in page.get_text_blocks(sort=True):
-            if str(l[0])[:4]==str(57.4): #00001525878906
-                EIN_masked=l[4][:-1]
-                break
-        if(EIN_masked):
-            break
-    
-    for page in doc:
-
-        selector=page.get_text_blocks()[0][4]
-        if(Form_8879_PE in selector):
-            search_and_replace(page,{"Brian A. Lang":"UpstreamConsulting.tax"},False)
-            if(not(EIN==None)):
-                search_and_replace(page,{"**-***":EIN},False)
-            
-        elif(Form_1065 in selector):
-            
+    try:
+        for page in doc:
             for l in page.get_text_blocks(sort=True):
-                if(str(l[0])[:6]=="198.76"):
+                if str(l[0])[:4]==str(57.4): #00001525878906
+                    EIN_masked=l[4][:-1]
+                    break
+            if(EIN_masked):
+                break
+        
+        for page in doc:
 
-                    if("U.S. Return of Partnership Income" in l[4]):
+            selector=page.get_text_blocks()[0][4]
+            if(Form_8879_PE in selector):
+                search_and_replace(page,{"Brian A. Lang":"UpstreamConsulting.tax"},False)
+                if(not(EIN==None)):
+                    search_and_replace(page,{"**-***":EIN},False)
+                
+            elif(Form_1065 in selector):
+                
+                for l in page.get_text_blocks(sort=True):
+                    if(str(l[0])[:6]=="198.76"):
 
-                        if(not(EIN==None)):
-                            search_and_replace(page,{EIN_masked:EIN},True)
-                        page.apply_redactions(images=PDF_REDACT_IMAGE_NONE)
+                        if("U.S. Return of Partnership Income" in l[4]):
 
-                        search_and_replace(page,{"**-***8651":"92-2981121"},False) 
+                            if(not(EIN==None)):
+                                search_and_replace(page,{EIN_masked:EIN},True)
+                            page.apply_redactions(images=PDF_REDACT_IMAGE_NONE)
 
-                        search_and_replace(page,{"Brian A. Lang":"UpstreamConsulting.tax","1125 Maxwell Lane, #1124 Hoboken,NJ 07030":"6586 Atlantic Ave Num 2066, Delray Beach, FL 33446"},False)
-                        search_and_replace(page,{"1125 Maxwell Lane, #1124 Hoboken,NJ 07030":"6586 Atlantic Ave Num 2066, Delray Beach, FL 33446"},False)
-                        search_and_replace(page,{"46-5698651":"92-2981121"},False) 
-                        search_and_replace(page,{"(646)580-2430":"(646)580-2430"},False)  
+                            search_and_replace(page,{"**-***8651":"92-2981121"},False) 
 
-        i+=1
-        page.apply_redactions(images=PDF_REDACT_IMAGE_NONE)
+                            search_and_replace(page,{"Brian A. Lang":"UpstreamConsulting.tax","1125 Maxwell Lane, #1124 Hoboken,NJ 07030":"6586 Atlantic Ave Num 2066, Delray Beach, FL 33446"},False)
+                            search_and_replace(page,{"1125 Maxwell Lane, #1124 Hoboken,NJ 07030":"6586 Atlantic Ave Num 2066, Delray Beach, FL 33446"},False)
+                            search_and_replace(page,{"46-5698651":"92-2981121"},False) 
+                            search_and_replace(page,{"(646)580-2430":"(646)580-2430"},False)  
+
+            i+=1
+            page.apply_redactions(images=PDF_REDACT_IMAGE_NONE)
             
+    except Exception as e:
+        print(str(e))
+        pass            
 
         
     doc.saveIncr()
@@ -134,7 +139,6 @@ def select_file():
         title='Open a file',
         initialdir=current_path,
         filetypes=filetypes)
-    
     
     if(len(filenames)!=0):
         
@@ -157,7 +161,7 @@ def select_file():
         
         showinfo(
             title='Selected File',
-            message="Modified the file successfuly"
+            message="Success"
         )
 
     else:
